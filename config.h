be5150394b3680db28bd7a7061113381cfd4cb89
iveyj <iveyj@hotmail.com>
2026-05-13 21:28:30 -0400

    ctrl + (actually =) ctrl - ctrl home zoom in, out, orig.
    st seems to need separate words for font name "Droid Sans M Nerd Font"

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DroidSansM Nerd Font Mono:pixelsize=32:antialias=true:autohint=true";
-static char *font2[] = { "DejaVu Sans Mono:pixelsize=32:antialias=true:autohint=true" };
+static char *font = "Droid Sans M Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+// static char *font2[] = { "DejaVu Sans Mono:pixelsize=32:antialias=true:autohint=true" };
+static char *font2[] = { "mono:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -252,9 +253,11 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
+	// { TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
+	// { TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ ControlMask,          XK_equal,        zoom,           {.f = +1} },
+	{ ControlMask,          XK_minus,       zoom,           {.f = -1} },
+	{ ControlMask,          XK_Home,        zoomreset,      {.f =  0} },
 	{ ControlMask|ShiftMask,XK_c,           clipcopy,       {.i =  0} },
 	{ ControlMask|ShiftMask,XK_C,           clipcopy,       {.i =  0} },
 	{ ControlMask|ShiftMask,XK_v,           clippaste,      {.i =  0} },
@@ -270,8 +273,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+	{ Mod1Mask|ControlMask, XK_k,           kscrollup,      {.i =  1} },
+	{ Mod1Mask|ControlMask, XK_j,           kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
