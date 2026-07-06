e011f2160584fbbae4151381faaff93076d3fa35
user@Latitude-7420-void <user@Latitude-7420-void>
2026-07-06 00:18:50 -0400

    ignore code 2026 -- synchronous update

--- st.c
+++ st.c
@@ -1930,6 +1930,8 @@ tsetmode(int priv, int set, const int *args, int narg)
 			case 2004: /* 2004: bracketed paste mode */
 				xsetmode(set, MODE_BRCKTPASTE);
 				break;
+			case 2026: /* 2026: synchronized output mode (IGNORED) */
+				break;
 			/* Not implemented mouse modes. See comments there. */
 			case 1001: /* mouse highlight mode; can hang the
 				      terminal by design when implemented. */
